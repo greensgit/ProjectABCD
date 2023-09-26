42e7b600ae3c84af4834bbc855b7f3782cf8479d
greensgit <rajgreensomr@gmail.com>
2023-09-26 13:19:12 +0530

    new code

--- src/org/dum/test/Abcd.java
+++ src/org/dum/test/Abcd.java
@@ -6,4 +6,8 @@ public class Abcd {
 	public static void main(String[] args) {
 		System.out.println("Demo class");
 	}
+	
+	private void namee() {
+		System.out.println("New branch created - Tasleem");
+	}
 }
